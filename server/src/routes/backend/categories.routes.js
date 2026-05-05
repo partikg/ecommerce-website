4c8b3dab24a0add5bf0b60210f99743c9dc567f4
Pratik Raut <pratik@example.com>
2026-05-05 18:14:52 +0530

    comit

--- server/src/routes/backend/categories.routes.js
+++ server/src/routes/backend/categories.routes.js
@@ -5,12 +5,12 @@ const upload = require('../../middleware/upload');
 
 module.exports = app => {
     route.post('/add', upload.single('image'), cateorycontroller.create);
-    route.post('/view', uploads.none(), cateorycontroller.view);
-    route.put('/delete', uploads.none(), cateorycontroller.delete);
+    route.post('/view', cateorycontroller.view);
+    route.put('/delete', cateorycontroller.delete);
     route.put('/update/:id', upload.single('image'), cateorycontroller.update);
-    route.put('/change-status', uploads.none(), cateorycontroller.changestatus);
-    route.put('/multi-delete', uploads.none(), cateorycontroller.multidelete);
-    route.post('/details/:id', uploads.none(), cateorycontroller.details);
+    route.put('/change-status', cateorycontroller.changestatus);
+    route.put('/multi-delete', cateorycontroller.multidelete);
+    route.post('/details/:id', cateorycontroller.details);
 
     app.use('/api/backend/categories', route);
 }
